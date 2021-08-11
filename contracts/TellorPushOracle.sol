f27c97483f754dceefd3eb50d37c985e3a4b22ef
cpondoc <chris.pondoc@comcast.net>
2021-08-11 12:33:36 -0400

    Finalize initial cleaning up of code and brainstorming testing

--- contracts/TellorPushOracle.sol
+++ contracts/TellorPushOracle.sol
@@ -3,16 +3,35 @@ pragma solidity >=0.8.0;
 
 import "usingtellor/contracts/UsingTellor.sol";
 import "./TellorPushUser.sol";
-import "hardhat/console.sol";
 
+/** 
+ @author Christopher Pondoc ༼ つ ◕_◕ ༽つ
+ @title TellorPushOracle
+ @dev Turns the Tellor oracle into a push oracle by adding functionality
+ to push data directly into smart contracts
+**/
 contract TellorPushOracle is UsingTellor {
+
+    // Functions
+    /**
+     * @dev Sets up UsingTellor oracle by initializing with address
+     * @param _tellorAddress address of Tellor oracle or Tellor Playground instance
+     */
     constructor(address payable _tellorAddress) UsingTellor(_tellorAddress) {}
 
-    // Essentially just makes sure that the data is pushed
-    function pushNewData(uint256 _tellorID, address userContract) external payable {
+    /**
+     * @dev Pushes data to a smart contract using the push oracle mechanism
+     * @param _tellorID the ID of the data the oracle pushed
+     * @param _userContract the smart contract to push data to -- must be a 
+     TellorUser contract
+     */
+    function pushNewData(uint256 _tellorID, address _userContract) external payable {
+        // Grab current value from Tellor oracle and ensure data was retrieved
         (bool ifRetrieve, uint256 value, ) = getCurrentValue(_tellorID);
         require(ifRetrieve == true, "Data was not retrieved!");
-        TellorPushUser tellorUser = TellorPushUser(userContract);
+
+        // Pushes data to Tellor user contract using receiveResult function
+        TellorPushUser tellorUser = TellorPushUser(_userContract);
         tellorUser.receiveResult(_tellorID, value);
     }
 }
---
+++ test/README.md
@@ -0,0 +1,9 @@
+# List of Potential Tests
+
+## End to End
+* Work on submitting a value, returning it, and making sure it's good
+    - Try with a combination of different IDs and values
+* Look at require statements
+    - Try making a call from another instance of TellorPushOracle
+    - Try with non-zero/negative value
+    - See if retrieve not possible, etc.
