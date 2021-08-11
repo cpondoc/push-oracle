2951319f99eb4b57915ea2afd11f2da3010c16c6
cpondoc <chris.pondoc@comcast.net>
2021-08-11 10:09:16 -0400

    Upgrade to 0.8.0 for Solidity

--- contracts/ITellorPushUser.sol
+++ contracts/ITellorPushUser.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0;
+pragma solidity >=0.8.0;
 
 interface ITellorPushUser {
     function receiveResult(uint256 _requestID, uint256 oracleValue) external;
