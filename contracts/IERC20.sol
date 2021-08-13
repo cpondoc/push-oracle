ce12a3461a2d8c0309c330f8c613b640994623f5
cpondoc <chris.pondoc@comcast.net>
2021-08-13 11:23:23 -0400

    Add ERC20 contracts and interface from Fellowship contract

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -1,22 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0;
 
-/** 
- @author Christopher Pondoc ༼ つ ◕_◕ ༽つ
- @title IERC20
- @dev Interface for standard ERC 20 Token
-**/
 interface IERC20 {
+    function transfer(address _to, uint256 _amount) external returns (bool);
 
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address account) external view returns (uint256);
-    function allowance(address owner, address spender) external view returns (uint256);
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _amount
+    ) external returns (bool);
 
-    function transfer(address recipient, uint256 amount) external returns (bool);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function balanceOf(address _addy) external returns (uint256);
 
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(address indexed owner, address indexed spender, uint256 value);
+    function balanceOfAt(address _addy, uint256 _block)
+        external
+        returns (uint256);
 }
